012ad46ed11879341912c0df2ea74e7e753ec2cc
Yuri Grillo <yuri.grillo@ascential.com>
2024-04-18 14:26:51 -0300

    chore: Reorganize imports on server.js

--- api-rest/server.js
+++ api-rest/server.js
@@ -1,8 +1,9 @@
+import "dotenv/config";
 import app from "./src/app.js";
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log("Servidor escutando!");
-    console.log("Acesse por http://localhost:3000")
+    console.log("Acesse por http://localhost:3000");
 });
