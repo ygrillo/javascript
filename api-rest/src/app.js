4b0e1b882ba00741710eac8ed6027c09e59d9697
Yuri Grillo <yuri.grillo@ascential.com>
2024-04-18 14:03:13 -0300

    feat: Update and delete objects array and connect to MongoDB Atlas

--- api-rest/src/app.js
+++ api-rest/src/app.js
@@ -1,4 +1,14 @@
 import express from "express";
+import connectToDatabase from "./config/db-connect.js";
+
+const connection = await connectToDatabase();
+
+connection.on("error", (error) => {
+    console.error("erro de conexão.", error);
+});
+connection.once("open", () => {
+    console.log("conexão com o banco feito com sucesso!");
+});
 
 const app = express();
 app.use(express.json());
@@ -30,7 +40,7 @@ app.get("/livros", (_, res) => {
 
 app.get("/livros/:id", (req, res) => {
     // :id é uma informação variável
-    const index = searchBook(req.params.id)
+    const index = searchBook(req.params.id);
     res.status(200).json(books[index]);
 });
 
@@ -40,9 +50,15 @@ app.post("/livros", (req, res) => {
 });
 
 app.put("/livros/:id", (req, res) => {
-    const index = searchBook(req.params.id)
-    books[index].title = req.body.title
-    res.status(200).json(books)
-})
+    const index = searchBook(req.params.id);
+    books[index].title = req.body.title;
+    res.status(200).json(books);
+});
+
+app.delete("/livros/:id", (req, res) => {
+    const index = searchBook(req.params.id);
+    books.splice(index, 1);
+    res.status(200).send(`Livro ID: ${req.params.id} REMOVIDO!`);
+});
 
 export default app;
