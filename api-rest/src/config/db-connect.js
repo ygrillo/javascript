5cf17d194132da79f9fcc00d610c555a1fe81381
Yuri Grillo <yuri.grillo@ascential.com>
2024-04-18 14:27:53 -0300

    feat: Change user and password of MongoDB to variable enviroments

--- api-rest/src/config/db-connect.js
+++ api-rest/src/config/db-connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectToDatabase() {
     mongoose.connect(
-        "mongodb+srv://yugrillo4:SPpAQGoGKezHn0nQ@cluster0.s9i5e8i.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0"
+        `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PSWD}@cluster0.s9i5e8i.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0`
     );
     return mongoose.connection;
 }
