880d29866583d5e667b3cea9e33b95a2e5330283
Yuri Grillo <yuri.grillo@ascential.com>
2024-04-18 18:08:43 -0300

    feat: Remove routes from app.js

--- api-rest/src/app.js
+++ api-rest/src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import connectToDatabase from "./config/db-connect.js";
-import book from "./models/book.js";
+import routes from "./routes/index.js";
 
 const connection = await connectToDatabase();
 
@@ -12,38 +12,6 @@ connection.once("open", () => {
 });
 
 const app = express();
-app.use(express.json());
-
-app.get("/", (_, res) => {
-    res.status(200).send("<h1>Curso de Node.js</h1>");
-});
-
-app.get("/livros", async (_, res) => {
-    const booksArray = await book.find({})
-    res.status(200).json(booksArray);
-});
-
-app.get("/livros/:id", (req, res) => {
-    // :id é uma informação variável
-    const index = searchBook(req.params.id);
-    res.status(200).json(books[index]);
-});
-
-app.post("/livros", (req, res) => {
-    books.push(req.body);
-    res.status(201).send("Livros cadastrado com sucesso!");
-});
-
-app.put("/livros/:id", (req, res) => {
-    const index = searchBook(req.params.id);
-    books[index].title = req.body.title;
-    res.status(200).json(books);
-});
-
-app.delete("/livros/:id", (req, res) => {
-    const index = searchBook(req.params.id);
-    books.splice(index, 1);
-    res.status(200).send(`Livro ID: ${req.params.id} REMOVIDO!`);
-});
+routes(app);
 
 export default app;
