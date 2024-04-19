602ebdbad6264304f7a48dd80fa1abe4d82227b7
Yuri Grillo <yuri.grillo@ascential.com>
2024-04-19 16:10:53 -0300

    feat: Update response in API root

--- api-rest/src/routes/index.js
+++ api-rest/src/routes/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import books from "./bookRoutes.js";
+import authors from "./authorRoutes.js";
+import publishers from "./publisherRoutes.js";
+
+const html = `<h1>Leia o README</h1>`
 
 const routes = (app) => {
-    app.route("/").get((req, res) => res.status(200).send("Curso de Node.js"));
+    app.route("/").get((req, res) => res.status(200).send(html));
 
-    app.use(express.json(), books)
+    app.use(express.json(), books, authors, publishers);
 };
 
 export default routes;
