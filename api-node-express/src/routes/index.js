83427b33bece2d9ae5819a73b0936f9d0530aa09
Yuri Grillo <ygrillo@tutanota.com>
2024-07-11 21:31:18 -0300

    feat: Add usuarios to routes index

--- api-node-express/src/routes/index.js
+++ api-node-express/src/routes/index.js
@@ -1,17 +1,14 @@
-import express from "express"
-import livros from "./livrosRoutes.js"
-import autores from "./autoresRoutes.js"
+import express from 'express'
+import autores from './autoresRoutes.js'
+import livros from './livrosRoutes.js'
+import usuarios from './usuariosRoutes.js'
 
-const routes = (app) => {
-  app.route("/").get((req, res) => {
-    res.status(200).send({titulo: "Curso de node"})
+const routes = app => {
+  app.route('/').get((req, res) => {
+    res.status(200).send({ titulo: 'Curso de node' })
   })
 
-  app.use(
-    express.json(),
-    livros,
-    autores
-  )
+  app.use(express.json(), livros, autores, usuarios)
 }
 
 export default routes
